b00c93c1721cce151e344f915a210182c14d4aca
guilin.li <1344714332@qq.com>
2021-06-19 13:19:02 +0800

    modify config

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:COOKIES-LGL/blog.github.io.git master:gh-pages
 
 cd -
